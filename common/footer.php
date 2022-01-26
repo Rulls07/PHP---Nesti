2b9b27e1a9ff6101fa57e1e865b28a54bfc37c0b
Raul Hernandez <rjhb1985@gmail.com>
2022-01-26 15:13:10 +0100

    C:/Program Files/Git/assets/img/

--- common/footer.php
+++ common/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer text-faded text-center py-5">
     <div class="container">
-        <p class="m-0 small">Copyright &copy; Your Website 2021</p>
+        <p class="m-0 small">Copyright &copy; HERNANDEZ Raul</p>
     </div>
 </footer>
