39f82e05a02eb4c33a31b0c7b1ca176184ec1d09
Raul Hernandez <rjhb1985@gmail.com>
2022-01-26 12:55:02 +0100

    first commit

---
+++ content/data_realisations.php
@@ -0,0 +1,17 @@
+<?php
+$etudiants = array(
+    'CDA2021-01' =>  ["nom" => "Julien GROSSI", "url" => "https://grossi.needemand.com/"],
+    'CDA2021-02' =>  ["nom" => "Jean Bernard BRIOIS", "url" => "https://briois.needemand.com/"],
+    'CDA2021-03' =>  ["nom" => "Slimane SLIMANE", "url" => "https://slimane.needemand.com/"],
+    'CDA2021-04' =>  ["nom" => "Patrick ARNAUD", "url" => "https://arnaud.needemand.com/"],
+    'CDA2021-05' =>  ["nom" => "Laurent COCHONNEAU", "url" => "https://cochonneau.needemand.com/"],
+    'CDA2021-06' =>  ["nom" => "Thibault MOURICHON", "url" => "https://mourichon.needemand.com/"],
+    'CDA2021-07' =>  ["nom" => "Raul HERNANDEZ", "url" => "https://hernandez.needemand.com/"],
+    'CDA2021-08' =>  ["nom" => "Stephan CELESTIN", "url" => "https://razafiasimanana.needemand.com/"],
+    'CDA2021-09' =>  ["nom" => "Pierre OLIVIER", "url" => "https://pierre-olivierb.github.io"],
+    'CDA2021-10' =>  ["nom" => "Yen Nhi  NGO", "url" => "https://hotongminh.needemand.com/"],
+    'CDA2021-11' =>  ["nom" => "Sylvie LINXE", "url" => "https://linxe.needemand.com/"],
+    'CDA2021-12' =>  ["nom" => "Young Hee KIM", "url" => "https://kim.needemand.com/"],
+    'CDA2021-13' =>  ["nom" => "Kevin SAN MARTINO", "url" => "https://cda.needemand.com/author/cda2020kevin/"],
+);
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('common/head.php'); ?>
+<?php include('content/swicth_content.php'); ?>
+
+<body>
+
+    <?php include('common/header.php'); ?>
+    <!-- Navigation-->
+    <?php include('common/navigation.php'); ?>
+    <?php include($text); ?>
+    <!-- Footer-->
+    <?php include('common/footer.php'); ?>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
